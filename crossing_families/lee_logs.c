6c36f182b1dc0165b6d5dfa06843220d2c77f7bf
Carlos Bulnes <carlos0bulnes@gmail.com>
2017-01-25 12:10:12 -0600

    Programa para obtener el maximo y minimo en los archivos de log

--- crossing_families/lee_logs.c
+++ crossing_families/lee_logs.c
@@ -1,19 +1,49 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
-{
-	FILE *file = fopen("logk1-3-10", "r");
-	int a, b;
-	char c;
+typedef struct cf{
+	int otype;
+	int ncrossings;
+}CrossingFamily;
+
+
+int main(int argc, char *argv[]){
+	FILE *file;
+	int otype, ncrossings;
+	CrossingFamily min, max;
+	char c, nombre[50];
+
+	do{
+		printf("Nombre del archivo: ");
+		scanf("%s", nombre);
+	}while((file = fopen(nombre, "r")) == NULL);
+
+
+
+	fscanf(file, "%d%c %d", &min.otype, &c, &min.ncrossings);
+
+	max.otype = min.otype;
+	max.ncrossings = min.ncrossings;
 
 	while(!feof(file)){
-		fscanf(file, "%d%c %d", &a, &c, &b);
+		//printf("%d %c %d\t", otype, c, ncrossings);
+		fscanf(file, "%d%c %d", &otype, &c, &ncrossings);
 
-		printf("%d %c %d\t", a, c, b);
+		if(ncrossings < min.ncrossings){
+			min.otype = otype;
+			min.ncrossings = ncrossings;
+		}
+
+		if(ncrossings > max.ncrossings){
+			max.otype = otype;
+			max.ncrossings = ncrossings;
+		}
+
 	}
 
+	printf("Minimo otype %d con %d Crossing Families\n", min.otype, min.ncrossings);
+	printf("Maximo otype %d con %d Crossing Families\n", max.otype, max.ncrossings);
 
-
+	fclose(file);
 
 	return 0;
 }
