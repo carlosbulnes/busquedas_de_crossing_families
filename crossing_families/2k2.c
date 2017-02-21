4345dc25e00320fadfec1ceadb70066fd9bee48d
Carlos Bulnes <carlos0bulnes@gmail.com>
2017-02-21 09:25:47 -0600

    Cambio en la forma de construir las 2k2

--- crossing_families/2k2.c
+++ crossing_families/2k2.c
@@ -3,11 +3,15 @@
 #include <string.h>
 #include "geom.h"
 
+typedef struct k2{
+	Segmento s1;
+	Segmento s2;
+}s2K2;
 
 int main(int argc, char *argv[]){
 	
 	int n, otypes, c = 1, i, j, k, l, m, bytes, nsegmentos, npuntos, inicio, kotypes;
-	int crossings = 0, ncrossings = 0, siempre_existe = 1, bandera;
+	int crossings = 0, ncrossings = 0, siempre_existe = 1, n2k2;
 	char order_type[50], etiqueta;
 
 	if(argc < 2){
@@ -23,9 +27,9 @@ int main(int argc, char *argv[]){
 		case 5: strcpy(order_type, "order_types/otypes05.b08"); otypes = 3; bytes = 1; nsegmentos = 10; break;
 		case 6: strcpy(order_type, "order_types/otypes06.b08"); otypes = 16; bytes = 1; nsegmentos = 15; break;
 		case 7: strcpy(order_type, "order_types/otypes07.b08"); otypes = 135; bytes = 1; nsegmentos = 21; break;
-		case 8: strcpy(order_type, "order_types/otypes08.b08"); otypes = 3315; bytes = 1; nsegmentos = 28; break;
-		case 9: strcpy(order_type, "order_types/otypes09.b16"); otypes = 158817; bytes = 2; nsegmentos = 36; break;
-		case 10: strcpy(order_type, "order_types/otypes10.b16"); otypes = 14309547; bytes = 2; nsegmentos = 45; break;
+		case 8: strcpy(order_type, "order_types/otypes08.b08"); otypes = 3315; bytes = 1; nsegmentos = 28; n2k2 = 378; break;
+		case 9: strcpy(order_type, "order_types/otypes09.b16"); otypes = 158817; bytes = 2; nsegmentos = 36; n2k2 = 630; break;
+		case 10: strcpy(order_type, "order_types/otypes10.b16"); otypes = 14309547; bytes = 2; nsegmentos = 45; n2k2 = 990; break;
 	}
 
 	FILE *file = fopen(order_type, "rb");
@@ -37,6 +41,7 @@ int main(int argc, char *argv[]){
 
 	Punto *puntos = (Punto*)malloc(sizeof(Punto)*npuntos);
 	Segmento *segmentos = (Segmento*)malloc(sizeof(Segmento)*nsegmentos);
+	s2K2 *a2k2 = (s2K2*)malloc(sizeof(s2K2)*n2k2);
 	Segmento srojos[2], sazules[2];
 
 
@@ -77,11 +82,39 @@ int main(int argc, char *argv[]){
 				segmentos[k].b = puntos[j];
 				segmentos[k].etiqueta[0] = puntos[i].etiqueta;
 				segmentos[k].etiqueta[1] = puntos[j].etiqueta;
+				segmentos[k].etiqueta[3] = '\0';
 				k++;
 			}	
 		}
 
+		// Construye todas las 2K2 con los n en 2 segmentos
+		for(i = 0, k = 0; i < nsegmentos; i++){
+			for(j = i+1; j < nsegmentos; j++){
+				if(!comparten_punto(segmentos[i], segmentos[j])){
+					a2k2[k].s1 = segmentos[i];
+					a2k2[k].s2 = segmentos[j];
+					//fprintf(log, "%s%s\n", a2k2[k].s1.etiqueta, a2k2[k].s2.etiqueta);
+					k++;
+				}
+			}
+		}
+
+		printf("hay %d 2k2 que no comparten punto\n", k); //return 0;
+		n2k2 = k;
 		crossings = 0;
+
+		for(i = 0; i < n2k2; i++){
+			for(j = i+1; j < n2k2; j++){
+				if(!comparten_punto(a2k2[i].s1, a2k2[j].s1) && !comparten_punto(a2k2[i].s1, a2k2[j].s2) && !comparten_punto(a2k2[i].s2, a2k2[j].s1) && !comparten_punto(a2k2[i].s2, a2k2[j].s2)){
+					if(interseccion(a2k2[i].s1, a2k2[j].s1) || interseccion(a2k2[i].s1, a2k2[j].s2) || interseccion(a2k2[i].s2, a2k2[j].s1) || interseccion(a2k2[i].s2, a2k2[j].s2)){
+						crossings++;
+						fprintf(log, "%s%s", a2k2[i].s1.etiqueta, a2k2[i].s2.etiqueta);
+						fprintf(log, "%s%s\n", a2k2[j].s1.etiqueta, a2k2[j].s2.etiqueta);
+					}
+				}
+			}
+		}
+
 
 		/*
 		// Imprime los puntos del order type
@@ -92,6 +125,7 @@ int main(int argc, char *argv[]){
 		printf("\n");
 		*/
 		//printf("--------------------otype %d-----------------------\n", (l/n)+1);
+		/*
 		for(i = 0; i < nsegmentos; i++){
 			srojos[0] = segmentos[i];
 			
@@ -139,14 +173,14 @@ int main(int argc, char *argv[]){
 
 									getchar();
 									getchar();
-								}*/
+								}
 							}
 						}
 					}
 				}
 			}
 		}
-
+		*/
 		ncrossings += crossings;
 		fprintf(log, "%d: %d\n", (l/n)+1, crossings);
 		if(((l/n)+1) % 500000 == 0) printf("%d/%d\n", (l/n)+1, otypes);
